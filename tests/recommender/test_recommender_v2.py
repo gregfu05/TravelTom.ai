140bc685c4bcf99eabd42fe243f93f85315b4911
Gregorio Santi Furnari <gfurnari.ieu2023@student.ie.edu>
2026-03-23 00:22:44 +0100

    black reformatting

--- tests/recommender/test_recommender_v2.py
+++ tests/recommender/test_recommender_v2.py
@@ -209,4 +209,3 @@ def test_city_not_found_returns_message() -> None:
     response = recommendor_v2.recommendation_tool(_query("bars in berlin"), catalog=df)
     assert len(response.results) == 1
     assert "don't have places for berlin" in response.results[0].features["name"]
- 
